fb5c328f79b473f383a2bfccb3584bf1bcd73821
娄松 <>
2025-01-09 14:41:10 +0800

    feat: input

--- components/ui/PromptInput/PromptInput.tsx
+++ components/ui/PromptInput/PromptInput.tsx
@@ -2,13 +2,13 @@
  * @Author: 娄松
  * @Date: 2025-01-08 17:08:11
  * @LastEditors: 娄松 
- * @LastEditTime: 2025-01-09 08:40:22
+ * @LastEditTime: 2025-01-09 14:10:01
  * @FilePath: \ask-ai\components\ui\PromptInput\PromptInput.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import * as React from "react";
 
-interface PromptInputProps extends React.ComponentProps<"textarea"> {
+interface PromptInputProps extends React.ComponentProps<"input"> {
     onSend: () => void
 }
 
@@ -17,24 +17,10 @@ const PromptInput = React.forwardRef<
   PromptInputProps
 >(({ value, onChange, onSend, ...props }, ref) => {
   return (
-    <div className="">
-      <div
-        className="bg-[#eee] rounded-[8px] p-[12px] mb-[30px] "
-        style={{}}
-      >
-        <div className="h-[60px]">
-          <textarea
-            className="outline-none w-full"
-            value={value}
-            onChange={onChange}
-            {...props}
-          />
-        </div>
-        <div className="flex justify-end text-[12px]" onClick={onSend}>
-          send
-        </div>
-      </div>
-    </div>
+    <form onSubmit={onSend}>
+    <input value={value} onChange={onChange} placeholder="saysomething" className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl" style={{border: '1px solid #ccc'}} />
+  </form>
+
   );
 });
 
